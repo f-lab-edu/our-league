3573904ea53aa55b13f0f374769dbb25193b9780
min <myun02302@gmail.com>
2024-05-15 16:59:07 +0900

    잘못된 merge로 create 메소드가 없어져서 다시 추가

--- src/main/java/com/minsproject/league/service/TeamService.java
+++ src/main/java/com/minsproject/league/service/TeamService.java
@@ -1,8 +1,14 @@
 package com.minsproject.league.service;
 
 import com.minsproject.league.dto.TeamSearchDTO;
+import com.minsproject.league.dto.request.TeamCreateRequest;
 import com.minsproject.league.dto.response.TeamResponse;
-import com.minsproject.league.repository.TeamsRepository;
+import com.minsproject.league.entity.Sports;
+import com.minsproject.league.entity.Team;
+import com.minsproject.league.exception.ErrorCode;
+import com.minsproject.league.exception.LeagueCustomException;
+import com.minsproject.league.repository.SportsRepository;
+import com.minsproject.league.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +18,16 @@ import java.util.List;
 @Service
 public class TeamService {
 
-    private final TeamsRepository teamsRepository;
+    private final TeamRepository teamRepository;
+    private final SportsRepository sportsRepository;
 
     public List<TeamResponse> getTeamList(TeamSearchDTO searchDTO) {
-        return teamsRepository.findByTeamIdGreaterThanOffsetId(searchDTO).stream().map(TeamResponse::fromEntity).toList();
+        return teamRepository.findByTeamIdGreaterThanOffsetId(searchDTO).stream().map(TeamResponse::fromEntity).toList();
+    }
+
+    public Long create(TeamCreateRequest request) {
+        Sports sports = sportsRepository.findById(request.getSportsId()).orElseThrow(() -> new LeagueCustomException(ErrorCode.SPORTS_NOT_FOUND));
+
+        return teamRepository.save(Team.fromDto(request, sports)).getTeamId();
     }
 }
