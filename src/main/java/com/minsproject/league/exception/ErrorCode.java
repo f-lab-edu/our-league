0bb436e1f4a5dc59df9331b7f00b8fe108a8927d
min <myun02302@gmail.com>
2024-06-02 20:00:00 +0900

    fix: 에러코드 추가 및 포맷 수정

--- src/main/java/com/minsproject/league/exception/ErrorCode.java
+++ src/main/java/com/minsproject/league/exception/ErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    // NOT_FOUND
     TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "팀을 찾을 수 없어요."),
 
     SPORTS_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 종목을 찾을 수 없어요."),
@@ -16,13 +17,22 @@ public enum ErrorCode {
 
     TEAM_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "팀 멤버를 찾을 수 없어요."),
 
+    DELETING_NOT_ALLOWED(HttpStatus.NOT_FOUND, "삭제할 수 없습니다."),
+
+    // UNAUTHORIZED
     MODIFICATION_NOT_ALLOWED(HttpStatus.UNAUTHORIZED, "수정할 수 없습니다."),
 
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "회원이나 비밀번호를 확인해주세요."),
 
-    DELETING_NOT_ALLOWED(HttpStatus.NOT_FOUND, "삭제할 수 없습니다."),
 
-    DUPLICATED_USER_EMAIL(HttpStatus.CONFLICT, "이미 가입한 이메일입니다."),
+    // BAD_REQUEST
+    DUPLICATED_USER_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입한 이메일입니다."),
+
+    TEAM_NOT_ACCEPTING_MEMBER(HttpStatus.BAD_REQUEST, "지금은 팀에 가입할 수 없습니다."),
+
+    ALREADY_IN_TEAM(HttpStatus.BAD_REQUEST, "이미 가입한 팀입니다.")
+
+
 
     ;
 
